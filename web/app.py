da8abe7a7d00dfb440a89481a4e7926a591e3299
Batya02 <magomedov.abdul20012@gmail.com>
2021-07-04 19:24:29 +0300

    changes

--- web/app.py
+++ web/app.py
@@ -5,20 +5,26 @@ from sqlalchemy import MetaData, create_engine
 
 from objects import globals
 
-globals.app = Flask(__name__)
-globals.app.config['SECRET_KEY'] = "top_programmer"
-globals.app.config['TEMPLATES_AUTO_RELOAD'] = True 
-
-#Database
-globals.db = Database("sqlite:///../_db/HH.sqlite")
-globals.metadata = MetaData()
-
-globals.db_engine = create_engine(str(globals.db.url))
-globals.metadata.create_all(globals.db_engine)
-
 import asyncio
 
+from json import loads
+
 async def main():
+
+    with open(r"config.json") as read_config:
+        config = loads(read_config.read())
+
+    globals.app = Flask(__name__)
+    globals.app.config['SECRET_KEY'] = config["SECRET_KEY"]
+    globals.app.config['TEMPLATES_AUTO_RELOAD'] = True 
+
+    #Database
+    globals.db = Database("sqlite:///../_db/HH.sqlite")
+    globals.metadata = MetaData()
+
+    globals.db_engine = create_engine(str(globals.db.url))
+    globals.metadata.create_all(globals.db_engine)
+
     import actions
 
 if __name__ == "__main__":
